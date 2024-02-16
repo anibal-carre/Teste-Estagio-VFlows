8a7f7c1c72161019a4cbd000cfb584b0ccb10887
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-02-16 09:46:14 -0300

    api cep added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(() => {
   console.log("ready");
+
+  const getAddress = (cep) => {
+    $.get("https://viacep.com.br/ws/" + cep + "/json/", (data) => {
+      $("#endereço").val(data.logradouro).prop("disabled", true);
+      $("#bairro").val(data.bairro).prop("disabled", true);
+      $("#municipio").val(data.localidade).prop("disabled", true);
+      $("#estado").val(data.uf).prop("disabled", true);
+    });
+  };
+
+  $("#cep").on("input", function () {
+    let cepValue = $(this).val();
+
+    if (cepValue && cepValue.length === 8) {
+      let cep = cepValue.replace(/\D/g, "");
+      getAddress(cep);
+    }
+  });
 });
